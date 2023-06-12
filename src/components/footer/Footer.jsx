20846a8d9fb56cfa991108b51094ede10407fff6
Maksym84 <burimskiy84@gmail.com>
2023-06-12 23:00:50 +0300

    footer styles

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,6 +8,7 @@ import { Link } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 function Footer() {
   // хук для відправки значення обраної мови до глобального сховища
@@ -26,8 +27,10 @@ function Footer() {
 
   const [t] = useTranslation(['translation']);
 
-  //масив мов на сторінці, передається до компоненту вибору мови для відображення значень
+  // масив мов на сторінці, передається до компоненту вибору мови для відображення значень
   const languages = ['English version', 'Українська версія'];
+  // перевірка ширини екрану для відображення блоків
+  const isWideScreen = useMediaQuery('(min-width:1200px)');
 
   // функція зміни мови, керує поведінкою useDispatch для зміни стану у сховищі
   function onChangeLanguage(language) {
@@ -43,13 +46,19 @@ function Footer() {
       <div className='container'>
         <div className='footer__conversion'>
           <p className='footer__conversionUpTitle'>
-            {t('footer.footer__conversionUpTitle')}
+            {isWideScreen
+              ? t('footer.footer__conversionUpTitle')
+              : t('footer.narrowScreen.footer__conversionUpTitle')}
           </p>
           <h2 className='footer__conversionTitle'>
-            {t('footer.footer__conversionTitle')}
+            {isWideScreen
+              ? t('footer.footer__conversionTitle')
+              : t('footer.narrowScreen.footer__conversionTitle')}
           </h2>
           <p className='footer__conversionSubTitle'>
-            {t('footer.footer__conversionSubTitle')}
+            {isWideScreen
+              ? t('footer.footer__conversionSubTitle')
+              : t('footer.narrowScreen.footer__conversionSubTitle')}
           </p>
           <BtnsBlock
             style={{ justifyContent: 'center' }}
@@ -57,172 +66,205 @@ function Footer() {
             addBtnClass={'mainBtn'}
             btnStyle={{ padding: '12px 32px' }}
             btnTo={'/signup'}
-            btnText={t('footer.BtnsBlock.btnText')}
+            btnText={
+              isWideScreen
+                ? t('footer.BtnsBlock.btnText')
+                : t('footer.narrowScreen.BtnsBlock.btnText')
+            }
             linkStyle={{ padding: '12px 4px 12px 16px' }}
             linkTo={'/pricing'}
             linkText={t('footer.BtnsBlock.linkText')}></BtnsBlock>
         </div>
         <div className='footer__mainLinks'>
-          <div className='footer__textBlock'>
-            <h3>
-              <Link to='/'>iTerms </Link>
-            </h3>
-            <p>{t('footer.footer__textBlock')}</p>
-          </div>
-          <div className='footer__linksBlock'>
-            <ul className='footer__linkList footer__linkList--row'>
-              <li>
-                <h4>{t('footer.footer__linksBlock.Company')}</h4>
-                <ul className='footer__linkList footer__linkList--column'>
-                  <li>
-                    <Link to='/'>
-                      {
-                        t('footer.footer__linkList.Company', {
-                          returnObjects: true,
-                        })[0]
-                      }
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to='/'>
-                      {
-                        t('footer.footer__linkList.Company', {
-                          returnObjects: true,
-                        })[1]
-                      }
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to='/'>
-                      {
-                        t('footer.footer__linkList.Company', {
-                          returnObjects: true,
-                        })[2]
-                      }
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to='/'>
-                      {
-                        t('footer.footer__linkList.Company', {
-                          returnObjects: true,
-                        })[3]
-                      }
-                    </Link>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <h4>{t('footer.footer__linksBlock.Products')}</h4>
-                <ul className='footer__linkList footer__linkList--column'>
-                  <li>
-                    <Link to='/'>
-                      {
-                        t('footer.footer__linkList.Products', {
-                          returnObjects: true,
-                        })[0]
-                      }
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to='/'>
-                      {
-                        t('footer.footer__linkList.Products', {
-                          returnObjects: true,
-                        })[1]
-                      }
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to='/'>
-                      {
-                        t('footer.footer__linkList.Products', {
-                          returnObjects: true,
-                        })[2]
-                      }
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to='/'>
-                      {
-                        t('footer.footer__linkList.Products', {
-                          returnObjects: true,
-                        })[3]
-                      }
-                    </Link>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <h4>{t('footer.footer__linksBlock.Support')}</h4>
-                <ul className='footer__linkList footer__linkList--column'>
-                  <li>
-                    <Link to='/'>
-                      {
-                        t('footer.footer__linkList.Support', {
-                          returnObjects: true,
-                        })[0]
-                      }
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to='/'>
-                      {
-                        t('footer.footer__linkList.Support', {
-                          returnObjects: true,
-                        })[1]
-                      }
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to='/'>
-                      {
-                        t('footer.footer__linkList.Support', {
-                          returnObjects: true,
-                        })[2]
-                      }
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to='/'>
-                      {
-                        t('footer.footer__linkList.Support', {
-                          returnObjects: true,
-                        })[3]
-                      }
-                    </Link>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </div>
+          <ul className='footer__linksBlock'>
+            <li>
+              <div className='footer__textBlock'>
+                <h3>
+                  <Link to='/'>iTerms </Link>
+                </h3>
+                {isWideScreen ? (
+                  <p>{t('footer.footer__textBlock')}</p>
+                ) : (
+                  <ul className='footer__linkList footer__linkList--column'>
+                    <li>
+                      <Link to='/pricing'>
+                        {t('footer.footer__infoLinks.linkList.to-pricing')}
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to='/'>
+                        {t('footer.footer__infoLinks.linkList.to-policy')}
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to='/'>
+                        {t('footer.footer__infoLinks.linkList.to-disclaimer')}
+                      </Link>
+                    </li>
+                    <li>
+                      <LanguageSelector
+                        currLang={currLang}
+                        options={languages}
+                        changeOption={onChangeLanguage}
+                      />
+                    </li>
+                  </ul>
+                )}
+              </div>
+            </li>
+            <li>
+              <h4>{t('footer.footer__linksBlock.Company')}</h4>
+              <ul className='footer__linkList footer__linkList--column'>
+                <li>
+                  <Link to='/'>
+                    {
+                      t('footer.footer__linkList.Company', {
+                        returnObjects: true,
+                      })[0]
+                    }
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/'>
+                    {
+                      t('footer.footer__linkList.Company', {
+                        returnObjects: true,
+                      })[1]
+                    }
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/'>
+                    {
+                      t('footer.footer__linkList.Company', {
+                        returnObjects: true,
+                      })[2]
+                    }
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/'>
+                    {
+                      t('footer.footer__linkList.Company', {
+                        returnObjects: true,
+                      })[3]
+                    }
+                  </Link>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <h4>{t('footer.footer__linksBlock.Products')}</h4>
+              <ul className='footer__linkList footer__linkList--column'>
+                <li>
+                  <Link to='/'>
+                    {
+                      t('footer.footer__linkList.Products', {
+                        returnObjects: true,
+                      })[0]
+                    }
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/'>
+                    {
+                      t('footer.footer__linkList.Products', {
+                        returnObjects: true,
+                      })[1]
+                    }
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/'>
+                    {
+                      t('footer.footer__linkList.Products', {
+                        returnObjects: true,
+                      })[2]
+                    }
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/'>
+                    {
+                      t('footer.footer__linkList.Products', {
+                        returnObjects: true,
+                      })[3]
+                    }
+                  </Link>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <h4>{t('footer.footer__linksBlock.Support')}</h4>
+              <ul className='footer__linkList footer__linkList--column'>
+                <li>
+                  <Link to='/'>
+                    {
+                      t('footer.footer__linkList.Support', {
+                        returnObjects: true,
+                      })[0]
+                    }
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/'>
+                    {
+                      t('footer.footer__linkList.Support', {
+                        returnObjects: true,
+                      })[1]
+                    }
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/'>
+                    {
+                      t('footer.footer__linkList.Support', {
+                        returnObjects: true,
+                      })[2]
+                    }
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/'>
+                    {
+                      t('footer.footer__linkList.Support', {
+                        returnObjects: true,
+                      })[3]
+                    }
+                  </Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
         </div>
         <div className='footer__infoLinks'>
           <span>© 2023 Terms Inc. {t('footer.footer__infoLinks.rights')}</span>
-          <ul className='footer__linkList footer__linkList--row'>
-            <li>
-              <Link to='/pricing'>
-                {t('footer.footer__infoLinks.linkList.to-pricing')}
-              </Link>
-            </li>
-            <li>
-              <Link to='/'>
-                {t('footer.footer__infoLinks.linkList.to-policy')}
-              </Link>
-            </li>
-            <li>
-              <Link to='/'>
-                {t('footer.footer__infoLinks.linkList.to-disclaimer')}
-              </Link>
-            </li>
-            <li>
-              <LanguageSelector
-                currLang={currLang}
-                options={languages}
-                changeOption={onChangeLanguage}
-              />
-            </li>
-          </ul>
+          {isWideScreen ? (
+            <ul className='footer__linkList footer__linkList--row'>
+              <li>
+                <Link to='/pricing'>
+                  {t('footer.footer__infoLinks.linkList.to-pricing')}
+                </Link>
+              </li>
+              <li>
+                <Link to='/'>
+                  {t('footer.footer__infoLinks.linkList.to-policy')}
+                </Link>
+              </li>
+              <li>
+                <Link to='/'>
+                  {t('footer.footer__infoLinks.linkList.to-disclaimer')}
+                </Link>
+              </li>
+              <li>
+                <LanguageSelector
+                  currLang={currLang}
+                  options={languages}
+                  changeOption={onChangeLanguage}
+                />
+              </li>
+            </ul>
+          ) : null}
         </div>
       </div>
     </footer>
